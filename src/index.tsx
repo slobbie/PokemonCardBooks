7dd129e06ca4d9b31fdcb4d4585a964861504267
slobbie <haeru9410@gmail.com>
2022-03-22 18:54:37 +0900

    :books: Docs readme 작성

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
-
-// react query 를 사용하기 위해서는 client 를 만들어야한다.
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <BrowserRouter>
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <App />
     </RecoilRoot>
   </BrowserRouter>,
 
